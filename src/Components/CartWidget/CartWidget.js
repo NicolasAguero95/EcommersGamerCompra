c7a0292f8f24683f7dc1317a7ebc1561df91e1bb
Coni2206 <nicolas.2206@hotmail.com>
2022-09-21 20:26:05 -0300

    PreEmtrega1NicolasAgueroAvalos

---
+++ src/Components/CartWidget/CartWidget.js
@@ -0,0 +1,14 @@
+import cart from "./assets/cart.svg"
+
+
+const CartWidtget = () => {
+    return (
+        <div>
+            <img src= {cart} alt="cart" />
+            0
+        </div>
+    )
+};
+
+
+export default CartWidtget
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import CartWidtget from "../CartWidget/CartWidget";
+import "./Navbar.css"
+
+const Navbar = () => {
+    return (
+        <nav style={{display: "flex", justifyContent: "space-around"}}>
+            <h1 class= "titulo"> Gamer Compra</h1>
+            <div class = "divButton">
+                <button>Productos</button>
+                <button>PC's</button>
+                <button>Celulares</button>
+                <button>Placas de Video</button>
+            </div>
+            <div class= "divCart">
+                <CartWidtget/>
+            </div>
+        </nav>
+    )
+};
+
+export default Navbar;
